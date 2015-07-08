f05b5be2dc8af5147e7e51f1f1a6fa8a276f0687
LunarDevelopment <development.lunar@gmail.com>
2015-07-08 16:44:18 +0100

    stuff, grunt build broken though

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,7 +11,13 @@ angular.module('lunardevApp')
   .controller('MainCtrl', function ($scope, $modal, $log) {
     $scope.pageClass = 'slide-animate';
     $scope.items = ['herpderp','herpderp'];
-  
+    var classes = [
+      'blue-grey', 'grey', 'brown', 'deep-orange', 'orange', 'amber', 'yellow', 'lime', 'light-green', 'green', 'teal', 'cyan', 'light-blue', 'blue', 'indigo', 'deep-purple', 'purple', 'pink', 'red'
+    ];
+    $scope.generateClass = function () {
+      var random = Math.floor(Math.random() * classes.length );
+      return 'panel-material-' + classes[random];
+    };
     $scope.open = function () {
 
       var modalInstance = $modal.open({
---
+++ app/scripts/directives/randompanelclass.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name lunardevApp.directive:randomPanelClass
+ * @description
+ * # randomPanelClass
+ */
+angular.module('lunardevApp')
+  .directive("randomPanelClass", function () {
+    return {
+      restrict: 'EA',
+      replace: false,
+      scope: {
+        ngClasses: "=ngRandomClass"
+      },
+      link: function (scope, elem, attr) {
+        //Add random background class to selected element
+        scope.ngClasses = [
+          'blue-grey', 'grey', 'brown', 'deep-orange', 'orange', 'amber', 'light-green', 'green', 'teal', 'cyan', 'light-blue', 'blue', 'indigo', 'deep-purple', 'purple', 'pink', 'red'
+        ];
+        elem.addClass('panel-material-' + scope.ngClasses[Math.floor(Math.random() * (scope.ngClasses.length))]);
+      }
+    }
+  });
