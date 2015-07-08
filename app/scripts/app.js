a85911e171fe940e973b600d2defc7afb46d4983
LunarDevelopment <development.lunar@gmail.com>
2015-07-08 11:44:21 +0100

    material design added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'flock.bootstrap.material'
   ])
     .config(function ($routeProvider) {
         $routeProvider
